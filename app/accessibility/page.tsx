6cf3aa593ba89737c1c9ff232b4baa67fcf216b9
Audu Martin <audumart@Audus-MacBook-Pro.local>
2026-06-23 22:22:24 -0500

    making UI changes

--- app/accessibility/page.tsx
+++ app/accessibility/page.tsx
@@ -20,8 +20,8 @@ function GridTile({ top, left }: { top: number; left: string }) {
   return (
     <div
       aria-hidden="true"
-      className="pointer-events-none absolute h-[1461px] w-[1513px] -translate-x-1/2 origin-top scale-[0.27] md:scale-[0.53] lg:scale-[0.71] xl:scale-[1]"
-      style={{ top, left, opacity: 0.11 }}
+      className="pointer-events-none absolute h-[1461px] w-[1513px] -translate-x-1/2 origin-top scale-[0.27] md:scale-[0.53] lg:scale-[0.71] xl:scale-[1] opacity-50 xl:opacity-[0.11]"
+      style={{ top, left }}
     >
       <div className="absolute h-[1049px] w-[1440px]" style={{ left: 37, top: 0 }}>
         <img src={A.hline} alt="" className="absolute inset-0 block size-full max-w-none" />
